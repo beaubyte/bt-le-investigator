603e3bf93679559618b552e5aceeaac1ce7f71b6
Beau <beau@mochabeau.red>
2025-11-11 09:27:01 -0600

    basic database stuff, queries work now

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <QBluetoothLocalDevice>
 #include <QBluetoothDeviceDiscoveryAgent>
 #include <QSqlDatabase>
+#include <QSqlQuery>
 #include "discovery.h"
 int main(int argc, char *argv[])
 {
@@ -13,11 +14,29 @@ int main(int argc, char *argv[])
     QString localDeviceName;
     qInfo() << "Connecting to the database server";
     QSqlDatabase db = QSqlDatabase::addDatabase("QPSQL");
-    db.setHostName("recprism");
+    db.setHostName("prism.panda-mirach.ts.net");
     db.setDatabaseName("investigator");
-    db.setUserName("oskar");
-    db.setPassword("TheDayWeMet");
+    db.setUserName("testuser");
+    db.setPassword("TheDayWeMet"); // production version will use offuscated password
+
     bool ok = db.open();
+    if (ok) {
+        qInfo() << "Connected to database successfully!";
+        QSqlQuery query("select macaddress, friendlyname from bluetooth.findable");
+        if (query.first() == true){
+            while (query.next()) {
+                QString macaddress = query.value(0).toString();
+                QString name = query.value(1).toString();
+                qInfo() << macaddress << name;
+            }
+        } else {
+            qWarning() << "!! Query returned no results";
+        }
+    } else {
+        qWarning() << "!! Failed to connect to database";
+    }
+
+
     QList<QBluetoothAddress> remotes;
     // please connect to me to the local bluetooth device :3
     if (localDevice.isValid()) {
